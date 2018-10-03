67204507de1c8bd8663dbbb844bf360a5b973f5c
Lukas S <SchalkLukas20@gmail.com>
2018-10-03 22:51:29 +0200

    Update Key.java

--- Algorithm/Key.java
+++ Algorithm/Key.java
@@ -5,12 +5,30 @@ package Algorithm;
  */
 
 public class Key {
-    private final static int[] permutedLeft_PC1 = new int[]{57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36};
-    private final static int[] permutedRight_PC1 = new int[]{63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4};
+    private final static int[] permutedLeft_PC1 = new int[]{
+            57, 49, 41, 33, 25, 17, 9,
+            1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27,
+            19, 11, 3, 60, 52, 44, 36
+    };
+
+    private final static int[] permutedRight_PC1 = new int[]{
+            63, 55, 47, 39, 31, 23, 15,
+            7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29,
+            21, 13, 5, 28, 20, 12, 4
+    };
+
     private final static int[][] permutedArray_PC1 = new int[][]{permutedLeft_PC1, permutedRight_PC1};
 
-    private final static int[] permuted_PC2 = new int[]{14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
-            41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
+    private final static int[] permuted_PC2 = new int[]{
+            14, 17, 11, 24, 1, 5, 3, 28,
+            15, 6, 21, 10, 23, 19, 12, 4,
+            26, 8, 16, 7, 27, 20, 13, 2,
+            41, 52, 31, 37, 47, 55, 30, 40,
+            51, 45, 33, 48, 44, 49, 39, 56,
+            34, 53, 46, 42, 50, 36, 29, 32
+    };
 
     private final static int[][] shifts = new int[][]{
             {1, 2, 9, 16}, /* 1 shift */
@@ -38,6 +56,8 @@ public class Key {
      * @param maxRound - Round limiter
      */
     public void generateSubkeys(int maxRound) {
+        if (maxRound <= 0) return;
+
         this.maxRound = maxRound;
         subkeys = new int[this.maxRound][48];
 
